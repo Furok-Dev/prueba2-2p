4d6c93f4c43b45349114c450b06db9efd79a10b6
Alfredo Catucuago <thefurok@gmail.com>
2021-01-12 13:02:16 -0500

    se me olvido hacer commits, programa casi terminado

--- index.js
+++ index.js
@@ -2,3 +2,22 @@
 
 const express = require('express');
 const app = express();
+const { config } = require('./config/config');
+const cashierApi = require('./routes/cashierRoutes');
+
+//parser
+app.use(express.json());
+
+//usar las rutas de la cajera
+cashierApi(app);
+
+app.get('/', (req, res) => {
+  res.json('Por favor ve a : /cashier ');
+});
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.error(new Error('No se pudo levantar el servidor'));
+  }
+  console.log(`Servidor corriendo en http://localhost:${config.port}`);
+});
