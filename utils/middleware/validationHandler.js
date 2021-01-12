27d4f456279062cc07694a46f984f4f99b6231ec
Alfredo Catucuago <thefurok@gmail.com>
2021-01-12 13:01:36 -0500

    middleware para la validacion de datos

---
+++ utils/middleware/validationHandler.js
@@ -0,0 +1,24 @@
+/**
+ * Este middleware lo usaremos para realizar
+ * la validacion de los datos
+ *
+ * Tambien vamos a usar una libreria llamada Joi
+ * que nos ayudara a dar el formato a los datos
+ * que solicitamos
+ */
+const boom = require('@hapi/boom');
+
+const joi = require('joi');
+
+function validate(data, schema) {
+  const { error } = joi.object(schema).validate(data);
+  return error;
+}
+
+function validationHandler(schema, check = 'body') {
+  return function (req, res, next) {
+    const error = validate(req[check], schema);
+    error ? next(boom.badRequest(error)) : next();
+  };
+}
+module.exports = validationHandler;
